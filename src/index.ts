47e3104fdce27c3fbcb0e083a0d4043b8bc5f504
Tim Shaker <tim@tmshkr.com>
2023-10-06 15:32:38 -0700

    progress

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,18 @@ import { ElasticBeanstalkClient } from "@aws-sdk/client-elastic-beanstalk";
 import { getApplicationVersion } from "./getApplicationVersion";
 import { getTargetEnv } from "./getTargetEnv";
 import { deploy } from "./deploy";
+import { swapCNAMES } from "./swapCNAMES";
 
 const inputs = {
   appName: core.getInput("app_name", { required: true }),
-  awsRegion: core.getInput("aws_region", { required: true }),
+  awsRegion: core.getInput("aws_region", { required: false }),
+  awsAccessKeyId: core.getInput("aws_access_key_id", { required: false }),
+  awsSecretAccessKey: core.getInput("aws_secret_access_key", {
+    required: false,
+  }),
+  awsSessionToken: core.getInput("aws_session_token", { required: false }),
   blueEnv: core.getInput("blue_env", { required: true }),
+  deploy: core.getBooleanInput("deploy", { required: true }),
   greenEnv: core.getInput("green_env", { required: true }),
   platformBranchName: core.getInput("platform_branch_name", {
     required: true,
@@ -18,6 +25,7 @@ const inputs = {
   productionCNAME: core.getInput("production_cname", { required: true }),
   sourceBundlePath: core.getInput("source_bundle_path", { required: false }),
   stagingCNAME: core.getInput("staging_cname", { required: true }),
+  swapCNAMES: core.getBooleanInput("swap_cnames", { required: true }),
   templateName: core.getInput("template_name", { required: false }),
   versionLabel: core.getInput("version_label", { required: true }),
   waitForCreateEnv: core.getBooleanInput("wait_for_create_env", {
@@ -27,20 +35,31 @@ const inputs = {
 
 export type ActionInputs = typeof inputs;
 
+const credentials = {
+  accessKeyId: inputs.awsAccessKeyId || process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey:
+    inputs.awsSecretAccessKey || process.env.AWS_SECRET_ACCESS_KEY,
+  sessionToken: inputs.awsSessionToken || process.env.AWS_SESSION_TOKEN,
+};
+const hasCredentials =
+  (credentials.accessKeyId && credentials.secretAccessKey) ||
+  credentials.sessionToken;
+
 export const client = new ElasticBeanstalkClient({
-  region: inputs.awsRegion,
-  // logger: console,
+  region: inputs.awsRegion || process.env.AWS_REGION!,
+  credentials: hasCredentials ? credentials : undefined,
 });
 
 async function run(inputs: ActionInputs) {
-  console.log({ inputs });
   const applicationVersion = await getApplicationVersion(inputs);
-  console.log({ applicationVersion });
   const targetEnv = await getTargetEnv(inputs);
-  console.log({ targetEnv });
-  // deploy to the target environment
-  await deploy(targetEnv, applicationVersion);
-  // swap the CNAMEs
+
+  if (inputs.deploy) {
+    await deploy(targetEnv, applicationVersion);
+    if (inputs.swapCNAMES) {
+      await swapCNAMES(inputs);
+    }
+  }
 }
 
 run(inputs);
