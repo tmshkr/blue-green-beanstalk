f9094c74ac16c8e55a7399f061294986c90536a3
Tim Shaker <tim@tmshkr.com>
2023-10-08 15:36:57 -0700

    update tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -4,33 +4,28 @@ import {
 } from "@aws-sdk/client-elastic-beanstalk";
 
 import { main } from "./main";
-import { ActionInputs } from "./inputs";
+const { randomBytes } = require("node:crypto");
 
-const inputs: ActionInputs = {
-  appName: `test-app-${Date.now()}`,
-  awsRegion: "us-west-2",
-  blueEnv: `my-blue-env-${Date.now()}`,
-  deploy: true,
-  greenEnv: `my-green-env-${Date.now()}`,
-  platformBranchName: "Docker running on 64bit Amazon Linux 2023",
-  productionCNAME: `blue-green-test-${Date.now()}`,
-  sourceBundlePath: undefined,
-  stagingCNAME: undefined,
-  swapCNAMES: true,
-  templateName: undefined,
-  terminateUnhealthyEnvironment: true,
-  versionLabel: "v1",
-};
-
-const client = new ElasticBeanstalkClient({ region: inputs.awsRegion });
+const client = new ElasticBeanstalkClient({ region: "us-west-2" });
 
 describe("checkInputs", () => {
+  const key = randomBytes(4).toString("hex");
   it("should reject with an error when blueEnv and greenEnv are the same", () => {
     expect(() =>
       main({
-        ...inputs,
-        blueEnv: "test",
-        greenEnv: "test",
+        appName: `test-app-${key}`,
+        awsRegion: "us-west-2",
+        blueEnv: `same-${key}`,
+        deploy: true,
+        greenEnv: `same-${key}`,
+        platformBranchName: "Docker running on 64bit Amazon Linux 2023",
+        productionCNAME: `blue-green-test-${key}`,
+        sourceBundlePath: undefined,
+        stagingCNAME: `blue-green-test-staging-${key}`,
+        swapCNAMES: true,
+        templateName: undefined,
+        terminateUnhealthyEnvironment: true,
+        versionLabel: `test-version-${key}`,
       })
     ).rejects.toThrow("blue_env and green_env must be different");
   });
@@ -38,40 +33,114 @@ describe("checkInputs", () => {
   it("should reject with an error when productionCNAME and stagingCNAME are the same", () => {
     expect(() =>
       main({
-        ...inputs,
-        productionCNAME: "test",
-        stagingCNAME: "test",
+        appName: `test-app-${key}`,
+        awsRegion: "us-west-2",
+        blueEnv: `my-blue-env-${key}`,
+        deploy: true,
+        greenEnv: `my-green-env-${key}`,
+        platformBranchName: "Docker running on 64bit Amazon Linux 2023",
+        productionCNAME: `same-${key}`,
+        sourceBundlePath: undefined,
+        stagingCNAME: `same-${key}`,
+        swapCNAMES: true,
+        templateName: undefined,
+        terminateUnhealthyEnvironment: true,
+        versionLabel: `test-version-${key}`,
       })
     ).rejects.toThrow("production_cname and staging_cname must be different");
   });
 });
 
 describe("main", () => {
-  it(
-    "should create a new EB environment with correct inputs",
-    async () => {
-      const preTest = await client.send(
-        new DescribeEnvironmentsCommand({
-          ApplicationName: inputs.appName,
-          EnvironmentNames: [inputs.blueEnv, inputs.greenEnv],
-        })
-      );
+  const key = randomBytes(4).toString("hex");
+  const inputs = {
+    appName: `test-app-${key}`,
+    awsRegion: "us-west-2",
+    blueEnv: `my-blue-env-${key}`,
+    deploy: true,
+    greenEnv: `my-green-env-${key}`,
+    platformBranchName: "Docker running on 64bit Amazon Linux 2023",
+    productionCNAME: `blue-green-test-${key}`,
+    sourceBundlePath: undefined,
+    stagingCNAME: `blue-green-test-staging-${key}`,
+    swapCNAMES: true,
+    templateName: undefined,
+    terminateUnhealthyEnvironment: true,
+    versionLabel: `test-version-${key}`,
+  };
 
-      expect(preTest.Environments).toHaveLength(0);
-      await main(inputs);
+  describe("no production environment exists", () => {
+    it(
+      "should create a new production EB environment when none exists",
+      async () => {
+        const preTest = await client.send(
+          new DescribeEnvironmentsCommand({
+            ApplicationName: inputs.appName,
+            EnvironmentNames: [inputs.blueEnv, inputs.greenEnv],
+          })
+        );
 
-      const postTest = await client.send(
-        new DescribeEnvironmentsCommand({
-          ApplicationName: inputs.appName,
-          EnvironmentNames: [inputs.blueEnv, inputs.greenEnv],
-        })
-      );
+        expect(preTest.Environments).toHaveLength(0);
+        await main(inputs);
 
-      expect(postTest.Environments).toHaveLength(1);
-      expect(postTest.Environments[0].CNAME).toEqual(
-        `${inputs.productionCNAME}.${inputs.awsRegion}.elasticbeanstalk.com`
-      );
-    },
-    1000 * 60 * 10
-  );
+        const postTest = await client.send(
+          new DescribeEnvironmentsCommand({
+            ApplicationName: inputs.appName,
+            EnvironmentNames: [inputs.blueEnv, inputs.greenEnv],
+          })
+        );
+
+        expect(postTest.Environments).toHaveLength(1);
+        expect(postTest.Environments[0].CNAME).toEqual(
+          `${inputs.productionCNAME}.${inputs.awsRegion}.elasticbeanstalk.com`
+        );
+      },
+      1000 * 60 * 10
+    );
+  });
+
+  describe("production environment already exists", () => {
+    it(
+      "should create a new staging EB environment and then swap the CNAMES",
+      async () => {
+        const preTest = await client.send(
+          new DescribeEnvironmentsCommand({
+            ApplicationName: inputs.appName,
+            EnvironmentNames: [inputs.blueEnv, inputs.greenEnv],
+          })
+        );
+
+        expect(preTest.Environments).toHaveLength(1);
+        const oldEnv = preTest.Environments[0];
+
+        await main(inputs);
+
+        const postTest = await client.send(
+          new DescribeEnvironmentsCommand({
+            ApplicationName: inputs.appName,
+            EnvironmentNames: [inputs.blueEnv, inputs.greenEnv],
+          })
+        );
+
+        expect(postTest.Environments).toHaveLength(2);
+
+        expect(
+          postTest.Environments.find(
+            (env) => env.EnvironmentName === oldEnv.EnvironmentName
+          ).CNAME
+        ).toEqual(
+          `${inputs.stagingCNAME}.${inputs.awsRegion}.elasticbeanstalk.com`
+        );
+
+        expect(
+          postTest.Environments.find(
+            (env) => env.EnvironmentName !== oldEnv.EnvironmentName
+          ).CNAME
+        ).toEqual(
+          `${inputs.productionCNAME}.${inputs.awsRegion}.elasticbeanstalk.com`
+        );
+      },
+      1000 * 60 * 10
+    );
+  });
 });
