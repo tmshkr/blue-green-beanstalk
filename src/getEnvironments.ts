a6c805c29de554e43946e275b2f9e057a9ce97b9
Tim Shaker <tim@tmshkr.com>
2023-10-25 12:04:59 -0700

    wip

--- src/getEnvironments.ts
+++ src/getEnvironments.ts
@@ -1,9 +1,10 @@
 import {
+  DescribeEnvironmentResourcesCommand,
   DescribeEnvironmentsCommand,
   ElasticBeanstalkClient,
   EnvironmentDescription,
 } from "@aws-sdk/client-elastic-beanstalk";
-import { ActionInputs } from "./inputs";
+import { ActionInputs, DeploymentStrategy } from "./inputs";
 
 export async function getEnvironments(
   client: ElasticBeanstalkClient,
@@ -20,9 +21,62 @@ export async function getEnvironments(
     })
   );
 
-  const prodDomain = `${inputs.productionCNAME}.${inputs.awsRegion}.elasticbeanstalk.com`;
-  return {
-    prodEnv: Environments.find((env) => env.CNAME === prodDomain),
-    stagingEnv: Environments.find((env) => env.CNAME !== prodDomain),
-  };
+  const res = { prodEnv: undefined, stagingEnv: undefined };
+
+  switch (inputs.strategy) {
+    case DeploymentStrategy.SharedALB:
+      // check that it is a valid shared alb
+      const loadBalancers = await Promise.all(
+        Environments.map((env) => {
+          return client
+            .send(
+              new DescribeEnvironmentResourcesCommand({
+                EnvironmentId: env.EnvironmentId,
+              })
+            )
+            .then((res) => res.EnvironmentResources.LoadBalancers[0]?.Name);
+        })
+      );
+      console.log(Environments);
+      console.log(loadBalancers);
+      if (loadBalancers.find((arn) => /:loadbalancer\/[^app]/.test(arn))) {
+        throw new Error("Only Application Load Balancers are supported");
+      }
+      if (loadBalancers.find((arn) => /:loadbalancer\/app\/awseb/.test(arn))) {
+        throw new Error(
+          "Dedicated Load Balancers created by Elastic Beanstalk are not supported"
+        );
+      }
+
+      const uniqueLoadBalancers = new Set(loadBalancers);
+      if (uniqueLoadBalancers.size > 1) {
+        throw new Error(
+          "All environments must share the same Application Load Balancer"
+        );
+      }
+
+      if (uniqueLoadBalancers.size === 0) {
+        // create alb
+      }
+
+      // get the tags for the default target group, which will have the id of the prodEnv
+      throw new Error("SharedALB is not yet supported");
+      const prodId = "e-n2rmjanmdh";
+      res.prodEnv = Environments.find((env) => env.EnvironmentId === prodId);
+      res.stagingEnv = Environments.find(
+        (env) => env.EnvironmentName !== res.prodEnv.EnvironmentName
+      );
+      break;
+
+    case DeploymentStrategy.SwapCNAMEs:
+      const prodDomain = `${inputs.productionCNAME}.${inputs.awsRegion}.elasticbeanstalk.com`;
+      res.prodEnv = Environments.find((env) => env.CNAME === prodDomain);
+      res.stagingEnv = Environments.find((env) => env.CNAME !== prodDomain);
+      break;
+
+    default:
+      throw new Error(`Unknown strategy: ${inputs.strategy}`);
+  }
+
+  return res;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { ElasticBeanstalkClient } from "@aws-sdk/client-elastic-beanstalk";
+import {
+  ElasticBeanstalkClient,
+  EnvironmentDescription,
+  DescribeEnvironmentsCommand,
+} from "@aws-sdk/client-elastic-beanstalk";
 import * as core from "@actions/core";
 
 import { getApplicationVersion } from "./getApplicationVersion";
@@ -6,9 +10,7 @@ import { getTargetEnv } from "./getTargetEnv";
 import { createEnvironment } from "./createEnvironment";
 import { deploy } from "./deploy";
 import { swapCNAMES } from "./swapCNAMES";
-import { ActionInputs, getCredentials } from "./inputs";
-import { handleSharedALB } from "./strategies/shared_alb/handleSharedALB";
-import { handleSwapCNAMEs } from "./strategies/swap_cnames/handleSwapCNAMEs";
+import { ActionInputs, DeploymentStrategy, getCredentials } from "./inputs";
 
 export async function main(inputs: ActionInputs) {
   try {
@@ -20,20 +22,72 @@ export async function main(inputs: ActionInputs) {
     const applicationVersion = await getApplicationVersion(client, inputs);
     let targetEnv = await getTargetEnv(client, inputs);
 
-    if (inputs.deploy) {
-      if (targetEnv) {
-        await deploy(client, inputs, targetEnv, applicationVersion);
-      } else {
-        targetEnv = await createEnvironment(client, inputs, applicationVersion);
-      }
-      if (inputs.promote && inputs.waitForEnvironment) {
-        await swapCNAMES(client, inputs);
-      }
-    }
+    targetEnv = await handleDeploymentStrategy(
+      client,
+      inputs,
+      targetEnv,
+      applicationVersion
+    );
 
-    core.setOutput("target_env", targetEnv?.EnvironmentName || "");
+    await setOutputs(client, targetEnv);
   } catch (err) {
     core.setFailed(err.message);
     return Promise.reject(err);
   }
+}
+
+async function handleDeploymentStrategy(
+  client,
+  inputs,
+  targetEnv,
+  applicationVersion
+) {
+  if (!inputs.deploy) {
+    return targetEnv;
+  }
+
+  if (targetEnv) {
+    await deploy(client, inputs, targetEnv, applicationVersion);
+  } else {
+    targetEnv = await createEnvironment(client, inputs, applicationVersion);
+  }
+
+  if (!inputs.waitForEnvironment) {
+    return targetEnv;
+  }
+
+  if (inputs.promote) {
+    switch (inputs.strategy) {
+      case DeploymentStrategy.SharedALB:
+        // updateListenerRule(client, inputs, targetEnv);
+        break;
+      case DeploymentStrategy.SwapCNAMEs:
+        await swapCNAMES(client, inputs);
+        break;
+      default:
+        throw new Error(`Unknown strategy: ${inputs.strategy}`);
+    }
+  }
+
+  return targetEnv;
+}
+
+async function setOutputs(
+  client: ElasticBeanstalkClient,
+  targetEnv: EnvironmentDescription
+) {
+  targetEnv = await client
+    .send(
+      new DescribeEnvironmentsCommand({
+        EnvironmentIds: [targetEnv.EnvironmentId],
+      })
+    )
+    .then(({ Environments }) => Environments[0]);
+
+  core.setOutput("target_env_cname", targetEnv?.CNAME || "");
+  core.setOutput("target_env_id", targetEnv?.EnvironmentId || "");
+  core.setOutput("target_env_json", JSON.stringify(targetEnv) || "");
+  core.setOutput("target_env_endpoint_url", targetEnv?.EndpointURL || "");
+  core.setOutput("target_env_name", targetEnv?.EnvironmentName || "");
+  core.setOutput("target_env_status", targetEnv?.Status || "");
 }
