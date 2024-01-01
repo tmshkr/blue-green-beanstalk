f9976f7f2544b94caef5c45218bc59b213bece6f
Tim Shaker <tim@tmshkr.com>
2024-01-01 12:03:16 -0800

    check for targetEnv in enableTerminationProtection

--- src/updateTerminationProtection.ts
+++ src/updateTerminationProtection.ts
@@ -5,6 +5,11 @@ import { cfnClient } from "./clients";
 export async function enableTerminationProtection(
   targetEnv: EnvironmentDescription
 ) {
+  if (!targetEnv) {
+    throw new Error(
+      "No target environment found. Cannot enable termination protection."
+    );
+  }
   const stackName = `awseb-${targetEnv.EnvironmentId}-stack`;
   console.log(
     `[${targetEnv.EnvironmentName}]: Enabling termination protection for stack ${stackName}...`
