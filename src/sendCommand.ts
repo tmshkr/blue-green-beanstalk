23c1eb9b05c184bbf85f63d1a954605bdb3a6e72
Tim Shaker <tim@tmshkr.com>
2024-08-17 10:34:36 -0700

    add waitForCommand

--- src/sendCommand.ts
+++ src/sendCommand.ts
@@ -1,8 +1,18 @@
 import { EnvironmentDescription } from "@aws-sdk/client-elastic-beanstalk";
-import { SendCommandCommand } from "@aws-sdk/client-ssm";
+import {
+  Command,
+  CommandStatus,
+  SendCommandCommand,
+  ListCommandsCommand,
+} from "@aws-sdk/client-ssm";
 import { ActionInputs } from "./inputs";
 import { ssmClient } from "./clients";
 
+/**
+ * Sends a shell script command to an Elastic Beanstalk environment.
+ * @param inputs - the inputs for the action.
+ * @param targetEnv - the target environment to send the command to.
+ */
 export async function sendCommand(
   inputs: ActionInputs,
   targetEnv: EnvironmentDescription
@@ -11,7 +21,7 @@ export async function sendCommand(
     throw new Error("No environment available to send command");
   }
 
-  const response = await ssmClient.send(
+  const { Command } = await ssmClient.send(
     new SendCommandCommand({
       Targets: [
         {
@@ -28,5 +38,45 @@ export async function sendCommand(
       TimeoutSeconds: 600,
     })
   );
-  console.log(response);
+
+  if (inputs.wait_for_command) {
+    await waitForCommand({ command: Command });
+  }
+}
+
+/**
+ * Waits for an SSM command to complete or fail.
+ * @param command - the command to wait for.
+ */
+async function waitForCommand({ command }: { command: Command }) {
+  while (
+    command.Status === CommandStatus.PENDING ||
+    command.Status === CommandStatus.IN_PROGRESS
+  ) {
+    const { CompletedCount, TargetCount } = command;
+    const percentComplete = (CompletedCount / TargetCount) * 100;
+    console.log(
+      `Command status: ${
+        command.Status
+      } - ${CompletedCount}/${TargetCount} (${percentComplete.toPrecision(
+        3
+      )}%) completed...`
+    );
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    command = await ssmClient
+      .send(new ListCommandsCommand({ CommandId: command.CommandId }))
+      .then(({ Commands }) => {
+        if (Commands && Commands.length > 0) {
+          return Commands[0];
+        }
+        throw new Error("Command not found");
+      });
+  }
+
+  if (command.Status === CommandStatus.SUCCESS) {
+    console.log(`Command completed:`, command);
+  } else {
+    console.error(`Command failed:`, command);
+    throw new Error("Command failed");
+  }
 }
